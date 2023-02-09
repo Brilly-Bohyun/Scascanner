728f997cbbd358bec0dc001b1b5d6a00d5d62bf2
ddukddaki <rlfrkdms1@naver.com>
2023-02-09 15:36:20 +0900

    feat: StudyCafe Entity 추가

---
+++ studycafe/src/main/java/com/scascanner/studycafe/domain/StudyCafe.java
@@ -0,0 +1,36 @@
+package com.scascanner.studycafe.domain;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.time.LocalTime;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class StudyCafe {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "study_cafe_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner_id")
+    private Owner owner;
+
+    private String name;
+    private Integer minUsingTime;
+    private LocalTime openTime;
+    private LocalTime closeTime;
+    private String address;
+    private String comment;
+
+
+}
