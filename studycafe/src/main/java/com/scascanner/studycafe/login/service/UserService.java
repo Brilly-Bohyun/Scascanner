6c492500c78e4fd1cb524938e973bd8ae4c18750
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-02-15 21:56:29 +0900

    Feat : Create Find Service function
    단건 조회와 전체 조회의 서비스 로직을 구현했습니다.

--- studycafe/src/main/java/com/scascanner/studycafe/login/service/UserService.java
+++ studycafe/src/main/java/com/scascanner/studycafe/login/service/UserService.java
@@ -4,15 +4,28 @@ import com.scascanner.studycafe.domain.User;
 import com.scascanner.studycafe.login.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
 
+    @Transactional
     public Long join(User user){
         userRepository.save(user);
         return user.getId();
+    }
+
+    public List<User> findAllUsers(){
+        return userRepository.findAll();
+    }
+
+    public User findOneUser(Long userId){
+        return userRepository.findOne(userId);
     }
 }
