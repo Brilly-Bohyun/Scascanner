8d505a8bec5760b5b841a27b780bf6b7af5927dd
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-02-15 23:07:05 +0900

    Feat : Create UserForm
    회원가입을 위해 필요한 폼을 위해 UserForm객체를 만들었습니다.

---
+++ studycafe/src/main/java/com/scascanner/studycafe/login/controller/UserForm.java
@@ -0,0 +1,31 @@
+package com.scascanner.studycafe.login.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+
+@Getter @Setter
+public class UserForm {
+
+    @NotEmpty(message = "사용자 이메일은 필수 입니다.")
+    @Email
+    private String email;
+
+    @NotEmpty(message = "사용자 비밀번호는 필수 입니다.")
+    @Size(min = 8, max = 20, message = "8자 이상 20자 이하로 작성해야 합니다.")
+    private String password;
+
+    @Size(min = 1, max = 10, message = "1자 이상 10자 이하로 작성해야 합니다.")
+    private String nickname;
+
+    @NotEmpty(message = "사용자 이름은 필수 입니다.")
+    private String name;
+
+    @Past
+    private LocalDate birthday;
+}
