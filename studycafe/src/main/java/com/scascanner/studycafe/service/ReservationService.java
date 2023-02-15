518815705f81cf13d409124adb42e8d661c04684
ddukddaki <rlfrkdms1@naver.com>
2023-02-16 02:36:52 +0900

    feat: 예약 시간 조회 ->  예약이 이미 차있는 시간대는 false 예약이 없는 시간대는 true로 map 생성

--- studycafe/src/main/java/com/scascanner/studycafe/service/ReservationService.java
+++ studycafe/src/main/java/com/scascanner/studycafe/service/ReservationService.java
@@ -5,12 +5,35 @@ import com.scascanner.studycafe.repository.ReservationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 @Service
 @RequiredArgsConstructor
 public class ReservationService {
 
     private final ReservationRepository reservationRepository;
+    private Map<Integer, Boolean> reservationTimes = new ConcurrentHashMap<>();
 
+    public Map<Integer, Boolean> reservationTimeStatus(LocalDate targetDate, LocalTime openTime, LocalTime closeTime) {
+        List<LocalDateTime[]> allPossibleReservation = reservationRepository.findAllImpossibleReservation(targetDate);
+
+        for (int i = openTime.getHour(); i < closeTime.getHour(); i++) {
+            reservationTimes.put(i, true);
+        }
+
+        for (LocalDateTime[] localDateTimes : allPossibleReservation) {
+            for (int i = localDateTimes[0].getHour(); i < localDateTimes[1].getHour(); i++) {
+                reservationTimes.put(i, false);
+            }
+        }
+
+        return reservationTimes;
+    }
     public void reserve(Reservation reservation) {
         reservationRepository.reserve(reservation);
     }
