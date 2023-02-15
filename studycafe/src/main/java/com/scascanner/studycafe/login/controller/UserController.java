23b71a3ddc0cb44d6563926f4526c95452cd0226
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-02-16 01:47:38 +0900

    Feat : Create join POST API
    회원가입 폼을 입력한 후의 API를 설계했다.
    필드의 오류가 하나라도 있을 경우 다시 회원가입 폼으로 가며, 가입에 성공할 경우 메인화면으로 리다이렉트한다.

--- studycafe/src/main/java/com/scascanner/studycafe/login/controller/UserController.java
+++ studycafe/src/main/java/com/scascanner/studycafe/login/controller/UserController.java
@@ -5,7 +5,12 @@ import com.scascanner.studycafe.login.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
 
 @Controller
 @RequiredArgsConstructor
@@ -17,5 +22,14 @@ public class UserController {
     public String createForm(Model model){
         model.addAttribute("userForm", new UserForm());
         return "users/createUserForm";
+    }
+
+    @PostMapping("/users/new")
+    public String create(@Valid UserForm userForm, BindingResult result){
+        if(result.hasErrors()){
+            return "users/createUserForm";
+        }
+        userService.join(userForm);
+        return "redirect:/";
     }
 }
