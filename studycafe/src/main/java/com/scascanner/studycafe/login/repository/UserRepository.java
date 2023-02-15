85664e9435816f8deddfe34f76899b148949e57b
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-02-15 21:06:24 +0900

    Feat : Create Find function
    단건 조회, 전체 조회, 이름으로 조회해서 User를 찾는 기능을 구현했습니다

--- studycafe/src/main/java/com/scascanner/studycafe/login/repository/UserRepository.java
+++ studycafe/src/main/java/com/scascanner/studycafe/login/repository/UserRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Slf4j
 @Repository
@@ -16,5 +17,20 @@ public class UserRepository {
 
     public void save(User user){
         em.persist(user);
+    }
+
+    public User findOne(Long id){
+        return em.find(User.class, id);
+    }
+
+    public List<User> findAll() {
+        return em.createQuery("select u from User u", User.class)
+                .getResultList();
+    }
+
+    public List<User> findByName(String name){
+        return em.createQuery("select u from User u where u.name = :name", User.class)
+                .setParameter("name", name)
+                .getResultList();
     }
 }
