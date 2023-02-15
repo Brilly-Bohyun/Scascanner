db5a70c20ef36525de522d96d2f1a5ddf2b92e69
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-02-16 01:46:13 +0900

    Refactor : Change the parameter
    원래는 Entity자체를 받아왔지만 Dto로 받아오는 것으로 수정했다. 이 과정을 통해 암호화도 할 수 있으며 컨트롤러 부분의 간소화가 가능해졌다.

--- studycafe/src/main/java/com/scascanner/studycafe/login/service/UserService.java
+++ studycafe/src/main/java/com/scascanner/studycafe/login/service/UserService.java
@@ -1,6 +1,7 @@
 package com.scascanner.studycafe.login.service;
 
 import com.scascanner.studycafe.domain.User;
+import com.scascanner.studycafe.login.dto.UserForm;
 import com.scascanner.studycafe.login.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,7 +17,8 @@ public class UserService {
     private final UserRepository userRepository;
 
     @Transactional
-    public Long join(User user){
+    public Long join(UserForm userForm){
+        User user = userForm.toEntity();
         userRepository.save(user);
         return user.getId();
     }
