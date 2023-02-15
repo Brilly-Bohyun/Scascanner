93398d6a5fdb36390e0855b8a798b2b3f5f864f7
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-02-15 21:35:59 +0900

    Feat : Add getter & setter
    getter와 setter를 임시적으로 추가하였습니다.

--- studycafe/src/main/java/com/scascanner/studycafe/domain/User.java
+++ studycafe/src/main/java/com/scascanner/studycafe/domain/User.java
@@ -1,7 +1,9 @@
 package com.scascanner.studycafe.domain;
 
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -12,6 +14,7 @@ import java.util.Date;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter @Setter
 public class User {
 
     @Id @GeneratedValue
