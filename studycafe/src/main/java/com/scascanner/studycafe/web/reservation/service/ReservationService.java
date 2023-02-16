28f4b3b731b09a5d326f968eafed2d7ea6694933
ddukddaki <rlfrkdms1@naver.com>
2023-02-16 23:58:37 +0900

    feat: @Transactional 추가

--- studycafe/src/main/java/com/scascanner/studycafe/web/reservation/service/ReservationService.java
+++ studycafe/src/main/java/com/scascanner/studycafe/web/reservation/service/ReservationService.java
@@ -1,9 +1,10 @@
-package com.scascanner.studycafe.web.service;
+package com.scascanner.studycafe.web.reservation.service;
 
 import com.scascanner.studycafe.domain.entity.reservation.Reservation;
 import com.scascanner.studycafe.domain.repository.ReservationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -13,6 +14,7 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ReservationService {
 
