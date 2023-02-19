30987559056e3054f4ba588c0e194d1e6c8656fc
kmw2378 <rlarla677@gmail.com>
2023-02-19 23:07:29 +0900

    feat : RoomRepository에 어노테이션 추가
    리포지토리에서 EntityManager 가 동시성 이슈가 존재할 경우를 대비하여 @PersistenceContext 를 통해 이를 해결

--- studycafe/src/main/java/com/scascanner/studycafe/domain/repository/RoomRepository.java
+++ studycafe/src/main/java/com/scascanner/studycafe/domain/repository/RoomRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Transactional(readOnly = true)
@@ -13,6 +14,7 @@ import java.util.List;
 @Repository
 public class RoomRepository {
 
+    @PersistenceContext
     private final EntityManager em;
 
     public void save(Room room) {
