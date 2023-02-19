157df15eef83374672f51034ab4d0a6be04fbeea
kmw2378 <rlarla677@gmail.com>
2023-02-19 22:34:58 +0900

    feat : StudyCafeRepository 추가
    DB에서 JPA를 통해 StudyCafe 엔티티를 가져오거나 저장하는 리포지토리 파일 추가

--- studycafe/src/main/java/com/scascanner/studycafe/domain/ReservationStatus.java
+++
@@ -1,5 +0,0 @@
-package com.scascanner.studycafe.domain;
-
-public enum ReservationStatus {
-    RESERVED,CANCELED
-}
---
+++ studycafe/src/main/java/com/scascanner/studycafe/domain/repository/StudyCafeRepository.java
@@ -0,0 +1,32 @@
+package com.scascanner.studycafe.domain.repository;
+
+import com.scascanner.studycafe.domain.entity.StudyCafe;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class StudyCafeRepository {
+
+    private final EntityManager em;
+
+    public void save(StudyCafe studyCafe) {
+
+        if (studyCafe.getId() == null) {
+            em.persist(studyCafe);  // DB에 없는 경우엔 id 값이 null
+        } else {
+            em.merge(studyCafe);    // 이미 DB에 등록되있는 경우
+        }
+    }
+
+    public StudyCafe findById(Long cafeId) {
+        return em.find(StudyCafe.class, cafeId);
+    }
+
+    public List<StudyCafe> findAll() {
+        return em.createQuery("select sc from StudyCafe sc", StudyCafe.class).getResultList();
+    }
+}
