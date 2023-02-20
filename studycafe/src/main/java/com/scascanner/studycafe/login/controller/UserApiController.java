6ef39397cfec79f67c9a908775c68b96c3abfd07
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-02-20 22:52:01 +0900

    feat: create joinApi
    회원가입에 사용될 api 컨트롤러를 만들었습니다. 회원가입에 성공할 때, id와 인적정보를 json형식으로 출력해냅니다.

---
+++ studycafe/src/main/java/com/scascanner/studycafe/login/controller/UserApiController.java
@@ -0,0 +1,35 @@
+package com.scascanner.studycafe.login.controller;
+
+import com.scascanner.studycafe.login.dto.UserForm;
+import com.scascanner.studycafe.login.dto.UserInfoDto;
+import com.scascanner.studycafe.login.service.UserService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.nio.charset.StandardCharsets;
+
+@RestController
+@RequiredArgsConstructor
+public class UserApiController {
+
+    private final UserService userService;
+
+    @PostMapping("/api/new-user")
+    public ResponseEntity<UserInfoDto> saveUser(@RequestBody @Valid UserForm userForm){
+        Long id = userService.join(userForm);
+        UserInfoDto userInfoDto = new UserInfoDto(id, userForm.getEmail(), userForm.getPassword(), userForm.getNickname(),
+                userForm.getName(), userForm.getBirthday());
+
+        HttpHeaders header = new HttpHeaders();
+        header.setContentType(new MediaType("application","json", StandardCharsets.UTF_8));
+
+        return new ResponseEntity<>(userInfoDto, HttpStatus.OK);
+    }
+}
