e39b3f9afe9bdf13d37479e39ab7e760894e9e7b
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-02-20 22:51:07 +0900

    feat: create userInfoDto
    json반환을 위해 고유 id와 인적정보를 담을 dto를 새로 생성함

---
+++ studycafe/src/main/java/com/scascanner/studycafe/login/dto/UserInfoDto.java
@@ -0,0 +1,33 @@
+package com.scascanner.studycafe.login.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+public class UserInfoDto {
+    private Long id;
+
+    @NotEmpty(message = "사용자 이메일은 필수 입니다.")
+    @Email
+    private String email;
+
+    @NotEmpty(message = "사용자 비밀번호는 필수 입니다.")
+    @Size(min = 8, max = 20, message = "8자 이상 20자 이하로 작성해야 합니다.")
+    private String password;
+
+    @Size(min = 1, max = 10, message = "1자 이상 10자 이하로 작성해야 합니다.")
+    private String nickname;
+
+    @NotEmpty(message = "사용자 이름은 필수 입니다.")
+    private String name;
+
+    @Past
+    private LocalDate birthday;
+}
