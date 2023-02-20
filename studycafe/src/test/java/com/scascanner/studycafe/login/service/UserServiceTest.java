3bb56a80940980d8d8371a071b4e61611ea4e2f5
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-02-20 21:29:56 +0900

    test: join test
    회원가입이 잘 이루어지는 지 테스트 코드를 작성했습니다.

---
+++ studycafe/src/test/java/com/scascanner/studycafe/login/service/UserServiceTest.java
@@ -0,0 +1,44 @@
+package com.scascanner.studycafe.login.service;
+
+import com.scascanner.studycafe.domain.User;
+import com.scascanner.studycafe.login.dto.UserForm;
+import com.scascanner.studycafe.login.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+
+@SpringBootTest
+@Transactional
+public class UserServiceTest{
+    @Autowired UserService userService;
+    @Autowired UserRepository userRepository;
+
+    @Test
+    public void 회원가입() throws Exception{
+        //Given
+
+        String email = "qhgus564@naver.com";
+        String password = "choi1234";
+        String nickname = "뽀";
+        String name = "최보현";
+        LocalDate birthday = LocalDate.of(2001, 10, 16);
+
+        //when
+        UserForm userForm1 = UserForm.builder()
+                .email(email)
+                .password(password)
+                .nickname(nickname)
+                .name(name)
+                .birthday(birthday)
+                .build();
+
+        Long savedId = userService.join(userForm1);
+
+        //then
+        Assertions.assertEquals(userRepository.findOne(savedId), userRepository.findByName(name));
+    }
+}
