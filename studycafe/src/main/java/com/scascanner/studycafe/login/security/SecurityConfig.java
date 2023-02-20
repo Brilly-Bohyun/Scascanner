60d8d8e397bc81cbdda5633e38cf9b99a4d6aa3b
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-02-20 20:14:29 +0900

    chore: change the settings
    spring security와 시험용 sql문이 자동으로 실행될 수 있도록 설정하였습니다.

--- studycafe/src/main/java/com/scascanner/studycafe/login/security/SecurityConfig.java
+++ studycafe/src/main/java/com/scascanner/studycafe/login/security/SecurityConfig.java
@@ -3,14 +3,27 @@ package com.scascanner.studycafe.login.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.formLogin().disable()
+                .csrf().disable()
+                .authorizeRequests()
+                .anyRequest().permitAll();
+
+        return http.build();
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
