c2ccd84535f0108d6290c9e3d86440adbe5550c3
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-02-20 21:29:25 +0900

    chore: remove unnecessary thing
    불필요해진 코드를 제거하여 수정하였습니다.

--- studycafe/src/main/java/com/scascanner/studycafe/login/service/UserService.java
+++ studycafe/src/main/java/com/scascanner/studycafe/login/service/UserService.java
@@ -3,7 +3,6 @@ package com.scascanner.studycafe.login.service;
 import com.scascanner.studycafe.domain.User;
 import com.scascanner.studycafe.login.dto.UserForm;
 import com.scascanner.studycafe.login.repository.UserRepository;
-import com.scascanner.studycafe.login.repository.UserRepositoryOld;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,6 +28,6 @@ public class UserService {
     }
 
     public User findOneUser(Long userId){
-        return userRepository.findById(userId).get();
+        return userRepository.findOne(userId);
     }
 }
