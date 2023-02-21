04b21ed8a9a8554c0ef38930628b459c7d1769ec
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-02-21 21:40:52 +0900

    feat: create withdrawl service
    회원 탈퇴 기능을 구현하였습니다. 또한, userId가 없을 경우 예외처리를 할 수 있도록 설정하였습니다.
    
    레포지토리 변경으로 인해 메소드명이 변경되었습니다.

--- studycafe/src/main/java/com/scascanner/studycafe/web/login/service/UserService.java
+++ studycafe/src/main/java/com/scascanner/studycafe/web/login/service/UserService.java
@@ -1,13 +1,15 @@
 package com.scascanner.studycafe.web.login.service;
 
 import com.scascanner.studycafe.domain.entity.User;
-import com.scascanner.studycafe.web.login.dto.UserForm;
 import com.scascanner.studycafe.domain.repository.UserRepository;
+import com.scascanner.studycafe.web.login.dto.UserForm;
+import com.scascanner.studycafe.web.login.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
@@ -28,13 +30,24 @@ public class UserService {
     }
 
     public User findOneUser(Long userId){
-        return userRepository.findOne(userId);
+        return userRepository.findById(userId)
+                .orElseThrow(()-> new UserNotFoundException(String.format("There is no Id : %s", userId)));
     }
 
     @Transactional
     public Long partialUpdate(Long id, UserForm userForm){
-        User user = userRepository.findOne(id);
+        User user = userRepository.findById(id).get();
         userRepository.save(user);
         return user.getId();
+    }
+
+    @Transactional
+    public User deleteById(Long userId) {
+        Optional<User> user = userRepository.findById(userId);
+        if(user.isPresent()){
+            userRepository.deleteById(userId);
+            return user.get();
+        }
+        return null;
     }
 }
