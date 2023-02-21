22605cb9a16332103e9b5267e84f2d43b359712a
ddukddaki <rlfrkdms1@naver.com>
2023-02-21 19:43:40 +0900

    feat: id 로 studycafe를 db에서 조회하는 기능 추가

--- studycafe/src/main/java/com/scascanner/studycafe/domain/repository/StudyCafeRepository.java
+++ studycafe/src/main/java/com/scascanner/studycafe/domain/repository/StudyCafeRepository.java
@@ -1,15 +1,18 @@
 package com.scascanner.studycafe.domain.repository;
 
+import com.scascanner.studycafe.domain.entity.StudyCafe;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.LocalTime;
 import java.util.List;
 
 @Repository
 public class StudyCafeRepository {
 
-    EntityManager em;
+    @PersistenceContext
+    private EntityManager em;
 
     public List<LocalTime> findStudyCafeOperationTime(Long studycafeId) {
 
@@ -17,5 +20,9 @@ public class StudyCafeRepository {
                 .setParameter("id", studycafeId)
                 .getResultList();
 
+    }
+
+    public StudyCafe findStudyCafeById(Long studyCafeId) {
+        return em.find(StudyCafe.class, studyCafeId);
     }
 }
