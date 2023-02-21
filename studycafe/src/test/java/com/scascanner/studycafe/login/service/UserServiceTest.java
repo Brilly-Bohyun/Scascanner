985082ccc1849fdc3315731ec6dc4755f043912d
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-02-22 00:35:06 +0900

    test: find user by email test
    유저의 이메일을 이용해 유저를 찾아내는 비즈니스 로직의 테스트를 작성하였고 성공했습니다.

--- studycafe/src/test/java/com/scascanner/studycafe/login/service/UserServiceTest.java
+++ studycafe/src/test/java/com/scascanner/studycafe/login/service/UserServiceTest.java
@@ -1,5 +1,6 @@
 package com.scascanner.studycafe.login.service;
 
+import com.scascanner.studycafe.domain.entity.User;
 import com.scascanner.studycafe.domain.repository.UserRepository;
 import com.scascanner.studycafe.web.login.dto.UserForm;
 import com.scascanner.studycafe.web.login.service.UserService;
@@ -22,7 +23,6 @@ public class UserServiceTest{
     @Test
     public void 회원가입() throws Exception{
         //Given
-
         String email = "qhgus564@naver.com";
         String password = "choi1234";
         String nickname = "뽀";
@@ -42,5 +42,29 @@ public class UserServiceTest{
 
         //then
         Assertions.assertEquals(userRepository.findByName(name).get(0), userRepository.findById(savedId).get());
+    }
+
+    @Test
+    public void 이메일로찾기() throws Exception{
+        String email = "qhgus564@naver.com";
+        String password = "choi1234";
+        String nickname = "뽀";
+        String name = "최보현";
+        LocalDate birthday = LocalDate.of(2001, 10, 16);
+
+        //when
+        UserForm userForm1 = UserForm.builder()
+                .email(email)
+                .password(password)
+                .nickname(nickname)
+                .name(name)
+                .birthday(birthday)
+                .build();
+
+        Long savedId = userService.join(userForm1);
+        User userByEmail = userService.findByEmail(email);
+
+        //then
+        Assertions.assertEquals(userRepository.findById(savedId).get(), userByEmail);
     }
 }
