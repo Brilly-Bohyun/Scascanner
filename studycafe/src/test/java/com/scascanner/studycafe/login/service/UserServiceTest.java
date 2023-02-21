1e896c80367afa528a53e7e6ab3fcc2c63b4541e
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-02-22 00:48:18 +0900

    test: login test
    로그인을 했을 경우 반환되는 id값과 회원가입을 했을 경우 반환되는 id값이 일치하는 지 확인하여 로그인의 정상 동작 유무를 테스트하였고 성공했습니다.

--- studycafe/src/test/java/com/scascanner/studycafe/login/service/UserServiceTest.java
+++ studycafe/src/test/java/com/scascanner/studycafe/login/service/UserServiceTest.java
@@ -3,14 +3,16 @@ package com.scascanner.studycafe.login.service;
 import com.scascanner.studycafe.domain.entity.User;
 import com.scascanner.studycafe.domain.repository.UserRepository;
 import com.scascanner.studycafe.web.login.dto.UserForm;
+import com.scascanner.studycafe.web.login.dto.UserLogIn;
 import com.scascanner.studycafe.web.login.service.UserService;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -30,22 +32,16 @@ public class UserServiceTest{
         LocalDate birthday = LocalDate.of(2001, 10, 16);
 
         //when
-        UserForm userForm1 = UserForm.builder()
-                .email(email)
-                .password(password)
-                .nickname(nickname)
-                .name(name)
-                .birthday(birthday)
-                .build();
-
-        Long savedId = userService.join(userForm1);
+        Long savedId = getId(email, password, nickname, name, birthday);
 
         //then
-        Assertions.assertEquals(userRepository.findByName(name).get(0), userRepository.findById(savedId).get());
+        assertEquals(userRepository.findByName(name).get(0), userRepository.findById(savedId).get());
     }
+
 
     @Test
     public void 이메일로찾기() throws Exception{
+        //Given
         String email = "qhgus564@naver.com";
         String password = "choi1234";
         String nickname = "뽀";
@@ -53,6 +49,40 @@ public class UserServiceTest{
         LocalDate birthday = LocalDate.of(2001, 10, 16);
 
         //when
+        Long savedId = getId(email, password, nickname, name, birthday);
+        User userByEmail = userService.findByEmail(email);
+
+        //then
+        assertEquals(userRepository.findById(savedId).get(), userByEmail);
+    }
+
+    @Test
+    public void 로그인하기() throws Exception{
+        //Given
+        String email = "qhgus564@naver.com";
+        String password = "choi1234";
+        String nickname = "뽀";
+        String name = "최보현";
+        LocalDate birthday = LocalDate.of(2001, 10, 16);
+
+        //when
+        Long savedId = getId(email, password, nickname, name, birthday);
+        Long logInedId = login(email, password);
+
+        //then
+        assertEquals(logInedId, savedId);
+    }
+
+    private Long login(String email, String password) {
+        UserLogIn userLogIn = UserLogIn.builder()
+                .email(email)
+                .password(password)
+                .build();
+        Long logInedId = userService.longIn(userLogIn);
+        return logInedId;
+    }
+
+    private Long getId(String email, String password, String nickname, String name, LocalDate birthday) {
         UserForm userForm1 = UserForm.builder()
                 .email(email)
                 .password(password)
@@ -62,9 +92,6 @@ public class UserServiceTest{
                 .build();
 
         Long savedId = userService.join(userForm1);
-        User userByEmail = userService.findByEmail(email);
-
-        //then
-        Assertions.assertEquals(userRepository.findById(savedId).get(), userByEmail);
+        return savedId;
     }
 }
