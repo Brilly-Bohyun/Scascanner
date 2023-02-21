e702dd0fc1eb0f00e4c89091b47f5b24d2e2474f
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-02-21 23:16:33 +0900

    create: findByEmail function
    로그인 기능을 구현하기 위해 email을 통해 사용자를 찾을 수 있는 기능을 구현했습니다.

--- studycafe/src/main/java/com/scascanner/studycafe/domain/repository/UserRepository.java
+++ studycafe/src/main/java/com/scascanner/studycafe/domain/repository/UserRepository.java
@@ -4,7 +4,9 @@ import com.scascanner.studycafe.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long>{
   List<User> findByName(String name);
+  Optional<User> findByEmail(String email);
 }
