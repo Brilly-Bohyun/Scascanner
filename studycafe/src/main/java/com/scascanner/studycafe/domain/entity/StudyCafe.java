774c912bc50cbc2058ccf4e2cfcac6cdd0d5b107
kmw2378 <rlarla677@gmail.com>
2023-02-27 00:03:30 +0900

    feat : StudyCafe @Builder 추가

--- studycafe/src/main/java/com/scascanner/studycafe/domain/entity/StudyCafe.java
+++ studycafe/src/main/java/com/scascanner/studycafe/domain/entity/StudyCafe.java
@@ -34,8 +34,16 @@ public class StudyCafe {
     private String address;
     private String comment;
 
-    public Long getId() {
-        return id;
+    @Builder
+    public StudyCafe(Long id, Owner owner, String name, Integer minUsingTime, LocalTime openTime, LocalTime closeTime, String address, String comment) {
+        this.id = id;
+        this.owner = owner;
+        this.name = name;
+        this.minUsingTime = minUsingTime;
+        this.openTime = openTime;
+        this.closeTime = closeTime;
+        this.address = address;
+        this.comment = comment;
     }
 
     public void update(String name, Integer minUsingTime, LocalTime openTime, LocalTime closeTime, String address, String comment) {
