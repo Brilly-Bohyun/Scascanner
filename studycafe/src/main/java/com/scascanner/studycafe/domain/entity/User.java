677ecb6b968b92604fdb7b963a7efe02b3060878
kmw2378 <rlarla677@gmail.com>
2023-02-28 13:11:07 +0900

    refactor: User 엔티티의 기본키 생성 전략을 IDENTITY로 수정

--- studycafe/src/main/java/com/scascanner/studycafe/domain/entity/User.java
+++ studycafe/src/main/java/com/scascanner/studycafe/domain/entity/User.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.time.LocalDate;
 
@@ -14,7 +15,8 @@ import java.time.LocalDate;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
     private Long id;
 
