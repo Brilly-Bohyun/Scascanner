ae1fc10c786ae1e77e4a21c832c008b4720108ec
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-03-03 12:01:46 +0900

    feat: 검증과 비밀번호 암호화 추가
    회원가입 시 필요할 검증과 비밀번호가 암호화되어 저장될 수 있도록 만들었습니다.

--- studycafe/src/main/java/com/scascanner/studycafe/web/login/dto/OwnerDto.java
+++ studycafe/src/main/java/com/scascanner/studycafe/web/login/dto/OwnerDto.java
@@ -6,17 +6,35 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OwnerDto {
 
+    @NotEmpty(message = "사용자 이메일은 필수 입니다.")
+    @Email
     private String email;
+
+    @NotEmpty(message = "사용자 비밀번호는 필수 입니다.")
+    @Size(min = 8, max = 20, message = "8자 이상 20자 이하로 작성해야 합니다.")
     private String password;
+
+    @Size(min = 1, max = 10, message = "1자 이상 10자 이하로 작성해야 합니다.")
     private String nickname;
+
+    @NotEmpty(message = "사용자 이름은 필수 입니다.")
     private String name;
+
+    @Past
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthday;
 
     @Builder
@@ -43,7 +61,7 @@ public class OwnerDto {
                 .name(name)
                 .email(email)
                 .nickname(nickname)
-                .password(password)
+                .password(new BCryptPasswordEncoder().encode(password))
                 .birthday(birthday)
                 .build();
     }
