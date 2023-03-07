146b73a005cda6e69a69b19bc7dea9aba3bae3bd
kmw2378 <rlarla677@gmail.com>
2023-03-07 14:39:40 +0900

    feat: 토스를 통해 결제 완료시 카드 정보에 대한 응답 API 결과를 담을 클래스 추가

---
+++ studycafe/src/main/java/com/scascanner/studycafe/web/pay/dto/toss/response/TossPayResponseCard.java
@@ -0,0 +1,16 @@
+package com.scascanner.studycafe.web.pay.dto.toss.response;
+
+public class TossPayResponseCard {
+
+    private Integer issuerCode;
+    private Integer acquirerCode;
+    private Long number;
+    private Integer installmentPlanMonth;
+    private String interestPayer;
+    private Integer approveNo;
+    private boolean useCardPoint;
+    private String cardType;
+    private String ownerType;
+    private String acquireStatus;
+    private Integer amount;
+}
