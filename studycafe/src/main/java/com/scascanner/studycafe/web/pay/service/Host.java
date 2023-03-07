f67296c0a5e3e02d51de0a668fbe87d1610f7c12
kmw2378 <rlarla677@gmail.com>
2023-03-07 14:45:21 +0900

    feat: 결제 요청을 보낼 URI 를 저장해놓은 열거체 추가

---
+++ studycafe/src/main/java/com/scascanner/studycafe/web/pay/service/Host.java
@@ -0,0 +1,6 @@
+package com.scascanner.studycafe.web.pay.service;
+
+public final class Host {
+    public static final String KAKAO_HOST = "https://kapi.kakao.com";
+    public static final String TOSS_HOST = "https://pay.toss.im";
+}
