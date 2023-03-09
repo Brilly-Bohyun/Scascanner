26d2c76c87ab6930eb1a993e6fd64a9915bf2ddb
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-03-10 02:25:17 +0900

    feat: JWT 기능 활용
    JWT기능을 활용하여 각 사용자마다 역할을 부여할 수 있기 때문에 관련 칼럼을 추가하였으며 UserDetails를 구현했습니다.

--- studycafe/src/main/java/com/scascanner/studycafe/domain/entity/User.java
+++ studycafe/src/main/java/com/scascanner/studycafe/domain/entity/User.java
@@ -1,19 +1,25 @@
 package com.scascanner.studycafe.domain.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,25 +27,75 @@ public class User {
     private Long id;
 
     private String email;
+    // 클라이언트 요청이 들어올 땐, 데이터를 받고 조회때는 제외되어 응답에 표시 안됨
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
     private String nickname;
     private String name;
     private LocalDate birthday;
 
-    public Long getId() {
-        return id;
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Builder.Default
+    private List<String> roles = new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
     }
 
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Override
     public String getPassword() {
         return password;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    // 계정이 만료가 안되었는지
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    // 계정이 잠기지 않았는지
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    // 계정 패스워드가 만료가 안됐는지
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    // 계정이 사용 가능한지
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
     @Builder
-    public User(String email, String password, String nickname, String name, LocalDate birthday) {
+    public User(String email, String password, String nickname, String name, LocalDate birthday,List<String> roles) {
         this.email = email;
         this.password = password;
         this.nickname = nickname;
         this.name = name;
         this.birthday = birthday;
+        this.roles = roles;
     }
+
+
 }
