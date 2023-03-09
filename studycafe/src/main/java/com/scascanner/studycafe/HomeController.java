d19c557047a65c3f7371f95d81ec8300c53f2a91
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-03-10 02:15:52 +0900

    refactor: JWT도입으로 인해 홈컨트롤러 초기버전으로 복귀
    JWT 도입으로 인해서 우선, 홈컨트롤러를 초기 버전으로 해두었습니다.

--- studycafe/src/main/java/com/scascanner/studycafe/HomeController.java
+++ studycafe/src/main/java/com/scascanner/studycafe/HomeController.java
@@ -1,12 +1,8 @@
 package com.scascanner.studycafe;
 
-import com.scascanner.studycafe.domain.entity.User;
-import com.scascanner.studycafe.domain.repository.UserRepository;
-import com.scascanner.studycafe.web.login.argumentresolver.Login;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
@@ -14,16 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequiredArgsConstructor
 public class HomeController {
 
-    private final UserRepository userRepository;
-
     @GetMapping("/")
-    public String home(@Login User loginUser, Model model){
-
-        if(loginUser == null){
-            return "home";
-        }
-
-        model.addAttribute("user", loginUser);
-        return "loginHome";
+    public String home(){
+        return "home";
     }
 }
