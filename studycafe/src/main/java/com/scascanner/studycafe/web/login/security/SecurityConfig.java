8959aa61ccae44915812c76c9bb9e23be6cc3b17
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-03-10 02:33:09 +0900

    feat: 웹 보안 설정 정보 수정 및 추가
    JWT 토큰을 통해 사용자를 구분하고 막아낼 수 있도록 웹 설정을 구현했습니다.

--- studycafe/src/main/java/com/scascanner/studycafe/web/login/security/SecurityConfig.java
+++ studycafe/src/main/java/com/scascanner/studycafe/web/login/security/SecurityConfig.java
@@ -1,11 +1,12 @@
 package com.scascanner.studycafe.web.login.security;
 
+import com.scascanner.studycafe.web.login.security.token.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,12 +15,31 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.formLogin().disable()
                 .csrf().disable()
+                // 로그인, 회원가입 API는 토큰이 없는 상태에서 요청해도 들어오게끔
                 .authorizeRequests()
-                .anyRequest().permitAll();
+                .antMatchers("/", "/users/**", "/owners/**").permitAll()
+                .anyRequest().hasRole("USER")
+                .and()
+                // 예외 처리
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+                // 세션을 사용하지 않음
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .apply(new JwtSecurityConfig(jwtTokenProvider));
+
 
         return http.build();
     }
