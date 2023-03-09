1dae1b62b71295f037e6df36051474806ecd4676
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-03-10 02:33:42 +0900

    feat: 예외 처리 기능 추가

--- studycafe/src/main/java/com/scascanner/studycafe/web/login/exception/UserNotFoundException.java
+++ studycafe/src/main/java/com/scascanner/studycafe/web/login/exception/UserNotFoundException.java
@@ -1,7 +1,16 @@
 package com.scascanner.studycafe.web.login.exception;
 
 public class UserNotFoundException extends RuntimeException{
+
+    public UserNotFoundException(String message, Throwable t){
+        super(message,t);
+    }
+
     public UserNotFoundException(String message) {
         super(message);
+    }
+
+    public UserNotFoundException(){
+        super();
     }
 }
