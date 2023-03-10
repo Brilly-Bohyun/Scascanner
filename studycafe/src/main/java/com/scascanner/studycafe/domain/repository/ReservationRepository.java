c82413f2897caf6f9c91cf156901c9caa1d03d3b
ddukddaki <rlfrkdms1@naver.com>
2023-03-10 14:55:02 +0900

    refactor: 쿼리 변경

--- studycafe/src/main/java/com/scascanner/studycafe/domain/repository/ReservationRepository.java
+++ studycafe/src/main/java/com/scascanner/studycafe/domain/repository/ReservationRepository.java
@@ -20,12 +20,14 @@ public class ReservationRepository {
 
     //날짜가 주어졌을 때 예약이 안되는 시간대를 반환
     public List<Object[]> findAllImpossibleReservation(LocalDate targetDate, Long studyCafeId, Long roomId) {
-        return em.createNativeQuery("select r.start_time, r.end_time from reservation r where r.date = ? and r.study_cafe_id = ? and r.room_id = ? and r.reservation_status = ?")
-                .setParameter(1, targetDate)
-                .setParameter(2, studyCafeId)
-                .setParameter(3, roomId)
-                .setParameter(4, RESERVED)
+
+        return em.createQuery("select r.startTime, r.endTime from Reservation r where r.date = :date and r.studyCafe.id = :studyCafeId and r.room.id = :roomId and r.reservationStatus = :status")
+                .setParameter("date", targetDate)
+                .setParameter("studyCafeId", studyCafeId)
+                .setParameter("roomId", roomId)
+                .setParameter("status", RESERVED)
                 .getResultList();
+
     }
     
     public void reserve(Reservation reservation) {
@@ -33,7 +35,7 @@ public class ReservationRepository {
     }
 
     public List<Reservation> findByUserId(Long userId) {
-        return em.createQuery("select r from Reservation r join r.user u on u.id = :userId")
+        return em.createQuery("select r from Reservation r where r.user.id = :userId")
                 .setParameter("userId", userId)
                 .getResultList();
     }
