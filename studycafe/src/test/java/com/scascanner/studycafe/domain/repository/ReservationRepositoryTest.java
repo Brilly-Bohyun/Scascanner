90b31e213204193cc9eb4b55b9f5140ea9248a2f
ddukddaki <rlfrkdms1@naver.com>
2023-03-17 00:59:33 +0900

    feat: reservationRepository test 추가 (findByUserId, findAllReservedTime)

--- studycafe/src/test/java/com/scascanner/studycafe/domain/repository/ReservationRepositoryTest.java
+++ studycafe/src/test/java/com/scascanner/studycafe/domain/repository/ReservationRepositoryTest.java
@@ -4,32 +4,25 @@ import com.scascanner.studycafe.domain.entity.Room;
 import com.scascanner.studycafe.domain.entity.StudyCafe;
 import com.scascanner.studycafe.domain.entity.User;
 import com.scascanner.studycafe.domain.entity.reservation.Reservation;
-import com.scascanner.studycafe.domain.entity.reservation.ReservationStatus;
+import com.scascanner.studycafe.fixture.ReservationFixture;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
+import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 
-import static com.scascanner.studycafe.domain.entity.reservation.ReservationStatus.CANCELED;
-import static com.scascanner.studycafe.domain.entity.reservation.ReservationStatus.RESERVED;
-import static com.scascanner.studycafe.fixture.ReservationFixture.RESERVATION_AEGIS_SIX;
-import static com.scascanner.studycafe.fixture.ReservationFixture.RESERVATION_TIMEU_FOUR;
-import static com.scascanner.studycafe.fixture.RoomFixture.FOUR_TIMEU;
-import static com.scascanner.studycafe.fixture.StudyCafeFixture.TIMEU;
+import static com.scascanner.studycafe.fixture.ReservationFixture.RESERVATION_CANCELED_19;
+import static com.scascanner.studycafe.fixture.ReservationFixture.RESERVATION_RESERVED_13;
+import static com.scascanner.studycafe.fixture.ReservationFixture.RESERVATION_RESERVED_15;
+import static com.scascanner.studycafe.fixture.RoomFixture.ROOM_FOUR;
+import static com.scascanner.studycafe.fixture.StudyCafeFixture.AEGIS;
 import static com.scascanner.studycafe.fixture.UserFixture.GAEUN;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.*;
 
 @SpringBootTest
+@Transactional
 class ReservationRepositoryTest {
 
     @Autowired ReservationRepository reservationRepository;
@@ -38,29 +31,50 @@ class ReservationRepositoryTest {
     public void id로_user가_예약한_목록_조회() {
         //given
         User user = GAEUN.생성();
-        StudyCafe studyCafe = TIMEU.생성();
-        Room room = FOUR_TIMEU.생성();
 
         List<Reservation> reservations = List.of(
-                RESERVATION_TIMEU_FOUR.사용자_예약상태_시간_생성(RESERVED, user, LocalTime.of(12, 0), LocalTime.of(14, 0)),
-                RESERVATION_AEGIS_SIX.사용자_예약상태_시간_생성(RESERVED, user, LocalTime.of(14,0), LocalTime.of(17,0)),
-                RESERVATION_TIMEU_FOUR.사용자_예약상태_시간_생성(CANCELED, user, LocalTime.of(12,0), LocalTime.of(14,0))
-        );
+                예약_저장(RESERVATION_RESERVED_13.사용자_예약_생성(user)),
+                예약_저장(RESERVATION_RESERVED_15.사용자_예약_생성(user)),
+                예약_저장(RESERVATION_CANCELED_19.사용자_예약_생성(user))
+       );
         //when
         List<Reservation> findReservations = reservationRepository.findByUserId(user.getId());
         //then
+        Assertions.assertThat(findReservations).hasSize(3);
         for (Reservation reservation : findReservations) {
             Assertions.assertThat(reservation).isIn(reservations);
         }
     }
 
+    private Reservation 예약_저장(Reservation reservation) {
+        reservationRepository.reserve(reservation);
+        return reservation;
+    }
+
     @Test
     public void 이미_예약이_되어있는_시간을_반환() {
         //given
+        StudyCafe aegis = AEGIS.생성();
+        Room room = ROOM_FOUR.스터디카페_생성(aegis);
+
+        for (ReservationFixture fixture : ReservationFixture.values()) {
+            예약_저장(fixture.스터디카페_룸_생성(aegis, room));
+
+        }
 
         //when
-
+        List<Object[]> allReservedReservation = reservationRepository.findAllReservedTime(LocalDate.now(), aegis.getId(), room.getId());
         //then
+        Assertions.assertThat(allReservedReservation).hasSize(2);
+
+        Assertions.assertThat(시간으로_변경(allReservedReservation.get(0)[0])).isEqualTo(13);
+        Assertions.assertThat(시간으로_변경(allReservedReservation.get(0)[1])).isEqualTo(15);
+        Assertions.assertThat(시간으로_변경(allReservedReservation.get(1)[0])).isEqualTo(15);
+        Assertions.assertThat(시간으로_변경(allReservedReservation.get(1)[1])).isEqualTo(18);
+    }
+
+    public int 시간으로_변경(Object time) {
+        return ((LocalTime) time).getHour();
     }
 
 
