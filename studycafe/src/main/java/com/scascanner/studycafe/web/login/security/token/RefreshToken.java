ec08f7cc25eb6a3ec45e04d951254e1489e92c64
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-03-17 01:04:26 +0900

    feat: refreshToken db에 저장하도록 구현
    refreshToken을 db에 저장할 수 있도록 entity와 repository를 구현했습니다.

---
+++ studycafe/src/main/java/com/scascanner/studycafe/web/login/security/token/RefreshToken.java
@@ -0,0 +1,30 @@
+package com.scascanner.studycafe.web.login.security.token;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@Builder
+@Entity
+@Table(name = "T_REFRESH_TOKEN")
+@NoArgsConstructor
+@AllArgsConstructor
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "REFRESH_TOKEN_ID", nullable = false)
+    private Long refreshTokenId;
+
+    @Column(name = "REFRESH_TOKEN", nullable = false)
+    private String refreshToken;
+
+    @Column(name = "KEY_EMAIL", nullable = false)
+    private String keyEmail;
+}
