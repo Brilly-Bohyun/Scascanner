d6c50f863557a05b81c02ae0e90e52d896d1e5dc
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-03-17 17:03:11 +0900

    refactor: dto 하나로 통합
    원래 두 dto의 필드값이 겹치는 부분이 많아서 하나로 통일시키고 싶었는데 관련 어노테이션을 찾아서 적용시킨 후 하나로 정리하였다.

--- studycafe/src/main/java/com/scascanner/studycafe/web/login/dto/UserInfoRequest.java
+++ studycafe/src/main/java/com/scascanner/studycafe/web/login/dto/UserInfoRequest.java
@@ -1,11 +1,12 @@
 package com.scascanner.studycafe.web.login.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.scascanner.studycafe.domain.entity.Role;
 import com.scascanner.studycafe.domain.entity.User;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.springframework.format.annotation.DateTimeFormat;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiParam;
+import lombok.*;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.validation.constraints.Email;
@@ -13,32 +14,44 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
-import java.util.Collections;
 
 @NoArgsConstructor
 @Getter
-@Setter
-public class UserForm {
+public class UserInfoRequest {
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    @ApiModelProperty(hidden = true)
+    private Long id;
+
     @NotEmpty(message = "사용자 이메일은 필수 입니다.")
+    @ApiModelProperty(example = "abcdef@naver.com", required = true)
+    @ApiParam(value = "사용자 아이디")
     @Email
     private String email;
 
     @NotEmpty(message = "사용자 비밀번호는 필수 입니다.")
+    @ApiModelProperty(example = "absdf123456", required = true)
+    @ApiParam(value = "사용자 비밀번호")
     @Size(min = 8, max = 20, message = "8자 이상 20자 이하로 작성해야 합니다.")
     private String password;
 
+    @ApiModelProperty(example = "말하는감자", required = true)
+    @ApiParam(value = "사용자 닉네임")
     @Size(min = 1, max = 10, message = "1자 이상 10자 이하로 작성해야 합니다.")
     private String nickname;
 
+    @ApiModelProperty(example = "홍길동", required = true)
+    @ApiParam(value = "사용자 이름")
     @NotEmpty(message = "사용자 이름은 필수 입니다.")
     private String name;
 
+    @ApiModelProperty(example = "1965-02-02", required = true)
+    @ApiParam(value = "사용자 생년월일")
     @Past
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthday;
 
     @Builder
-    public UserForm(String email, String password, String nickname, String name, LocalDate birthday) {
+    public UserInfoRequest(Long id, String email, String password, String nickname, String name, LocalDate birthday) {
+        this.id = id;
         this.email = email;
         this.password = password;
         this.nickname = nickname;
@@ -53,7 +66,7 @@ public class UserForm {
                 .nickname(nickname)
                 .name(name)
                 .birthday(birthday)
-                .roles(Collections.singletonList("ROLE_USER"))
+                .roles(Role.USER)
                 .build();
     }
 }
