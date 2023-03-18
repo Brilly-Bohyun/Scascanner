0e84345ca9f97a95956380b593c0dcd6aaa2f8fa
kmw2378 <rlarla677@gmail.com>
2023-03-18 21:03:13 +0900

    feat: 토스페이먼츠 클라이언트 키, 시크릿 키를 모아놓은 객체
    - 이 값들은 절대로 외부에 노출되서는 안됨

---
+++ studycafe/src/main/java/com/scascanner/studycafe/web/pay/service/ApiKey.java
@@ -0,0 +1,6 @@
+package com.scascanner.studycafe.web.pay.service;
+
+public class ApiKey {
+    public static final String CLIENT_KEY = "test_ck_0Poxy1XQL8RgmqMKe7k37nO5Wmlg";
+    public static final String SECRET_KEY = "test_sk_JQbgMGZzorzeDPE9E9vVl5E1em4d";
+}
