8518a891e1b28334fb925603925ce6befd4074fc
ddukddaki <rlfrkdms1@naver.com>
2023-03-18 13:09:57 +0900

    test: 스터디 카페 추가 해서 생성하는 기능 추가

--- studycafe/src/test/java/com/scascanner/studycafe/fixture/RoomFixture.java
+++ studycafe/src/test/java/com/scascanner/studycafe/fixture/RoomFixture.java
@@ -9,6 +9,8 @@ public enum RoomFixture {
     ROOM_FOUR(4),
     ROOM_SIX(6);
 
+
+
     private final int headCount;
 
     RoomFixture(int headCount) {
@@ -26,6 +28,13 @@ public enum RoomFixture {
 
     public Room 스터디카페_생성(StudyCafe studyCafe) {
         return 기본_정보_생성()
+                .studyCafe(studyCafe)
+                .build();
+    }
+
+    public Room 스터디카페_생성(StudyCafe studyCafe, Long id){
+        return 기본_정보_생성()
+                .id(id)
                 .studyCafe(studyCafe)
                 .build();
     }
