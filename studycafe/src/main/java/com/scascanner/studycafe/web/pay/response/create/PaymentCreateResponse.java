05ab5c576bb5b2d9ab7b2b66b12e2c94533fbbe0
kmw2378 <rlarla677@gmail.com>
2023-03-18 21:39:50 +0900

    feat: 예약 정보와 결제 수단을 통해 만들어진 요청정보에 대한 응답값, 토스페이먼츠에게 보낼 결제 요청 정보를 담은 api 추가

---
+++ studycafe/src/main/java/com/scascanner/studycafe/web/pay/response/create/PaymentCreateResponse.java
@@ -0,0 +1,50 @@
+package com.scascanner.studycafe.web.pay.response.create;
+
+import com.scascanner.studycafe.domain.entity.payment.Payment;
+import com.scascanner.studycafe.web.pay.PaymentMethod;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class PaymentCreateResponse {
+
+    private PaymentMethod method;
+    private Integer amount;
+    private String orderId;
+    private String orderName;
+    private String customerEmail;
+    private String customerName;
+    private String successUrl;
+    private String failUrl;
+    private String createDate;
+    private String paySuccessYn;
+
+    @Builder
+    public PaymentCreateResponse(PaymentMethod method, Integer amount, String orderId, String orderName, String customerEmail, String customerName, String successUrl, String failUrl, String createDate, String paySuccessYn) {
+        this.method = method;
+        this.amount = amount;
+        this.orderId = orderId;
+        this.orderName = orderName;
+        this.customerEmail = customerEmail;
+        this.customerName = customerName;
+        this.successUrl = successUrl;
+        this.failUrl = failUrl;
+        this.createDate = createDate;
+        this.paySuccessYn = paySuccessYn;
+    }
+
+    public static PaymentCreateResponse of(Payment payment) {
+        return PaymentCreateResponse.builder()
+                .method(payment.getMethod())
+                .amount(payment.getAmount())
+                .orderId(payment.getOrderId())
+                .orderName(payment.getOrderName())
+                .customerEmail(payment.getCustomerEmail())
+                .customerName(payment.getCustomerName())
+                .successUrl("http://localhost:8080/payment/confirm")
+                .failUrl("http://localhost:8080/payment/fail")
+                .build();
+    }
+}
