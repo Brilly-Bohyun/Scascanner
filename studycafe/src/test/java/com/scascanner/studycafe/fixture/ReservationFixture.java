68211e96fbf1db6aa79087c1a2d400ccb0f4c382
ddukddaki <rlfrkdms1@naver.com>
2023-03-18 13:09:32 +0900

    test: 스터디 카페 추가 해서 생성하는 기능 추가

--- studycafe/src/test/java/com/scascanner/studycafe/fixture/ReservationFixture.java
+++ studycafe/src/test/java/com/scascanner/studycafe/fixture/ReservationFixture.java
@@ -19,6 +19,7 @@ public enum ReservationFixture {
     RESERVATION_RESERVED_13(LocalDate.now(), LocalTime.of(13, 0), LocalTime.of(15, 0), RESERVED),
     RESERVATION_RESERVED_15(LocalDate.now(), LocalTime.of(15, 0), LocalTime.of(18, 0), RESERVED),
     RESERVATION_CANCELED_19(LocalDate.now(), LocalTime.of(19, 0), LocalTime.of(23, 0), CANCELED),
+    RESERVATION_RESERVED_FULL(LocalDate.now(), LocalTime.of(10, 0), LocalTime.of(23, 0), RESERVED),
     ;
 
     private final LocalDate reservationDate;
@@ -37,6 +38,12 @@ public enum ReservationFixture {
         return 기본_정보_생성().build();
     }
 
+    public Reservation 생성(Long id) {
+        return 기본_정보_생성()
+                .id(id)
+                .build();
+    }
+
     private ReservationBuilder 기본_정보_생성() {
         return Reservation.builder()
                 .startTime(this.startTime)
@@ -53,6 +60,17 @@ public enum ReservationFixture {
 
     public Reservation 스터디카페_룸_생성(StudyCafe studyCafe, Room room) {
         return 기본_정보_생성()
+                .studyCafe(studyCafe)
+                .room(room)
+                .build();
+    }
+
+    public Reservation 스터디카페_룸_날짜_생성(StudyCafe studyCafe, Room room, LocalDate date) {
+        return Reservation.builder()
+                .startTime(this.startTime)
+                .endTime(this.endTime)
+                .reservationStatus(this.reservationStatus)
+                .date(date)
                 .studyCafe(studyCafe)
                 .room(room)
                 .build();
